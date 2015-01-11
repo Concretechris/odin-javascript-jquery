cbf18f0c3bd356931089c90523ddefa60cb56120
Concretechris <chris.hall@richmond.edu>
2015-01-11 06:39:10 -0500

    update index.html, style.css and script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+  $("body").append("<div class = 'new_container' width='960'></div>");
+  
+  for (var a = 1; a <= 16; a++)
+  {
+  $(".new_container").append("<div class = 'container'></div>");
+  $(".container").height(960/16 - 2);
+  }
+  for (var b = 1; b <= 16; b++)
+  {
+  $(".container").append("<div class = 'cell'></div>");
+  $(".cell").height(960/16 - 4);
+  $(".cell").width(960/16 - 4);  
+  } 
+$("#reset").click(function(){
+    $(".container").remove();
+    $(".cell").remove();
+    var col = prompt("Please enter the number of columns: ");
+    var rows = prompt("Please enter the number rows: ");
+    
+    for (var i = 1; i < rows; i++)
+    {
+      $d = $("<div class = 'container'></div>");
+      $(".new_container").append($d);
+      $(".container").height(960/rows - 2);
+    }
+    for (var j = 1; j <= col; j++)
+    {
+      $c = $("<div class = 'cell'></div>");
+      $(".container").append($c);
+        $(".cell").width(960/col - 4);
+        $(".cell").height(960/col - 4);
+    }
+      $(".cell").mouseenter(function(){
+        $(this).addClass("black");
+    });
+});
+  $(".cell").mouseenter(function(){
+    $(this).addClass("black");
+  });
+      $("#clear").click(function(){
+        $(".cell").removeClass("black");
+  });
+});
